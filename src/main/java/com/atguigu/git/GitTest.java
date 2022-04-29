c6581f1364af4157f05b52a4b260a09e032df8c2
Reusful <Reusful@bvb>
2022-04-29 22:47:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Classname GitTest
+ * @Description TODO
+ * @Date 2022/4/29 22:40
+ * @Created by M.Reus
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
